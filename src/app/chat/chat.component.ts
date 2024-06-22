2b2d3463511c71bb9ec49394dadc132c3b55f736
Muhammad Naeem <muhammad.naeem@techlogix.com>
2024-06-22 23:31:56 +0500

    styling anf fixes

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,5 +1,6 @@
 // src/app/chat/chat.component.ts
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chat',
@@ -7,13 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat.component.css'],
 })
 export class ChatComponent {
+
   messages: string[] = [];
   newMessage: string = '';
 
+  constructor(private router: Router){}
   sendMessage() {
+
     if (this.newMessage.trim()) {
       this.messages.push(this.newMessage);
       this.newMessage = '';
     }
+  }
+  logout() {
+    this.router.navigate(['/']);
   }
 }
